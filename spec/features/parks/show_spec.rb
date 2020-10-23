81d951d8f4452024c7da6e96aa190cc372c2de5e
Shaun James <ShaunDaneJames@Gmail.com>
2020-10-23 10:30:20 -0600

    Adds test for list all rides and the parks average rating

--- spec/features/parks/show_spec.rb
+++ spec/features/parks/show_spec.rb
@@ -5,14 +5,32 @@ describe "As a visitor" do
     it "I see the name and price of admission for that park" do
       park = Park.create!(name: "6 Flags",
                           price: 50.00)
-      ride_1 = Ride.create!(name: "Lightning Racer",
-                            rating: 9,
-                            park_id: park.id)
 
       visit "/parks/#{park.id}"
 
       expect(page).to have_content("#{park.name}")
       expect(page).to have_content("$#{park.price}")
+    end
+
+    it "I see the names of each ride and the average rating" do
+      park = Park.create!(name: "6 Flags",
+                          price: 50.00)
+      ride_1 = Ride.create!(name: "Lightning Racer",
+                            rating: 9,
+                            park_id: park.id)
+      ride_2 = Ride.create!(name: "Storm Runner",
+                            rating: 7,
+                            park_id: park.id)
+      ride_3 = Ride.create!(name: "The Great Bear",
+                            rating: 8,
+                            park_id: park.id)
+
+      visit "/parks/#{park.id}"
+
+      expect(page).to have_content(ride_1.name)
+      expect(page).to have_content(ride_2.name)
+      expect(page).to have_content(ride_3.name)
+      expect(page).to have_content(park.average_rating)
     end
   end
 end
